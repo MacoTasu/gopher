4e3f5a1a6499e42a7851fc60b3bbc9926063c1df
MacoTasu <maco.tasu@gmail.com>
2015-08-15 00:41:59 +0900

    25％以下だったら立ち上げさせない

--- mirage/mirage.go
+++ mirage/mirage.go
@@ -18,8 +18,8 @@ func (m *Mirage) Launch() (string, error) {
 		return "", err
 	}
 
-	if percentage <= 0.4 {
-		return "", fmt.Errorf(fmt.Sprintf("Can't launch. AvailableMemory: %f %", percentage))
+	if percentage <= 0.25 {
+		return "", fmt.Errorf(fmt.Sprintf("Can't launch. AvailableMemory: %d %", percentage*100))
 	}
 
 	conf := config.LoadConfig()
